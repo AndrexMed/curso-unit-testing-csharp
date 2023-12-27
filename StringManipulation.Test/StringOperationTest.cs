9c190a816df9a0f9df827496154ff39a56e95272
AndrexMed <giovanniandrex@gmail.com>
2023-12-26 20:18:50 -0500

    Tipos de Assert en xUnit

--- StringManipulation.Test/StringOperationTest.cs
+++ StringManipulation.Test/StringOperationTest.cs
@@ -1,17 +1,46 @@
-﻿using Xunit;
-
-namespace StringManipulation.Test
+﻿namespace StringManipulation.Test
 {
     public class StringOperationTest
     {
         [Fact]
         public void ConcatenateStrings()
         {
+            //Arrange
             var stringOperation = new StringOperations();
 
+            //Act
             var result = stringOperation.ConcatenateStrings("Hello","Platzi");
 
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
             Assert.Equal("Hello Platzi", result); //Comprobar
+        }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            //Arrage
+            var stringOp = new StringOperations();
+
+            //Act
+            var result = stringOp.IsPalindrome("Ama");
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            //Arrage
+            var stringOp = new StringOperations();
+
+            //Act
+            var result = stringOp.IsPalindrome("hello");
+
+            //Assert
+            Assert.False(result);
         }
     }
 }
