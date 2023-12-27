55669a7ced604b870815c74e5e2918688d7e8eed
AndrexMed <giovanniandrex@gmail.com>
2023-12-26 19:35:38 -0500

    First Test

---
+++ StringManipulation.Test/StringOperationTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace StringManipulation.Test
+{
+    public class StringOperationTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var stringOperation = new StringOperations();
+
+            var result = stringOperation.ConcatenateStrings("Hello","Platzi");
+
+            Assert.Equal("Hello Platzi", result); //Comprobar
+        }
+    }
+}
